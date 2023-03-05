350b0a82fb909f17b0f79063b2f558a4ef38aa14
cyborg-geass <95498589+cyborg-geass@users.noreply.github.com>
2023-03-05 12:14:11 +0530

    Add files via upload

---
+++ er.py
@@ -0,0 +1,25 @@
+# def summ(x, y, z=4):
+#     return x+y+z
+
+# print(summ(1, y=4))
+# class base1:
+#     pass
+# class base2:
+#     coffee= True
+# class base3(base1 , base2):
+#     pass
+
+# print('er.py' in dir())
+
+# a =0
+# for i in range(10):
+#     if i==2:
+#         pass
+#     else:
+#         a = i
+# print(a)
+x = ["1", "2", "15", "-7", "300"]
+print(sorted(x))
+num=5
+has= num>10
+print(has)
